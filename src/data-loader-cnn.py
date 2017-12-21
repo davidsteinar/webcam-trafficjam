46ef8b9025a62449c4c04c5702e7bf254913e2aa
dsteinar <davidsteinar@gmail.com>
2017-12-21 11:42:32 +0000

    correct labels, working model

--- src/data-loader-cnn.py
+++ src/data-loader-cnn.py
@@ -14,7 +14,7 @@ CHANNELS = 3
 
 # Reading the dataset
 def read_images(dataset_path, batch_size, mode):
-    imagepaths, labels = [], []
+    image_paths, labels = [], []
         # Read dataset file
     if mode == 'train':
         dataset_path_mod = dataset_path + 'image_sets/training.txt'
@@ -27,8 +27,14 @@ def read_images(dataset_path, batch_size, mode):
         data = f.read().splitlines()
         
     for d in data:
-        imagepaths.append(dataset_path+'images/'+d)
-        labels.append(1) #################################################################################not correct
+        image_paths.append(dataset_path+'images/'+d)
+        label_path = dataset_path+'images/'+d.replace('.jpg','.txt')
+        
+        with open(label_path, 'r', encoding='utf-8') as f:
+            for i, l in enumerate(f):
+                pass
+            
+        labels.append(i+1)
 
     # Convert to Tensor
     imagepaths = tf.convert_to_tensor(imagepaths, dtype=tf.string)
@@ -44,8 +50,8 @@ def read_images(dataset_path, batch_size, mode):
     image = tf.image.resize_images(image, [IMG_HEIGHT, IMG_WIDTH])
 
     # Normalize
-    image = image * 1.0/127.5 - 1.0 ################################################################### sketchy
-
+    #image = image * 1.0/127.5 - 1.0 ################################################################### sketchy
+    image = tf.image.per_image_standardization(image) #better ?
     # Create batches
     X, Y = tf.train.batch([image, label], batch_size=batch_size,
                           capacity=batch_size * 8)#,
@@ -58,7 +64,7 @@ learning_rate = 0.001
 num_steps = 1000
 batch_size = 3
 display_step = 100
-dropout = 0.5
+dropout = 0.2
 
 # Build the data input
 X_train_batch, Y_train_batch = read_images(DATASET_PATH, batch_size, mode='train')
@@ -66,41 +72,52 @@ X_test_batch, Y_test_batch   = read_images(DATASET_PATH, batch_size, mode='test'
 X_val_batch, Y_val_batch     = read_images(DATASET_PATH, batch_size, mode='validation')
 
 # Create model
+# Create model
 def conv_net(x, reuse=False):
     # Define a scope for reusing the variables
     with tf.variable_scope('ConvNet', reuse=reuse):
-        '''
-        # Convolution Layer with 32 filters and a kernel size of 5
-        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
-
-        # Convolution Layer with 32 filters and a kernel size of 5
-        conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
-        # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
-
-        # Flatten the data to a 1-D vector for the fully connected layer
-        fc1 = tf.contrib.layers.flatten(conv2)
-
-        # Fully connected layer (in contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 1024)
-        # Apply Dropout (if is_training is False, dropout is not applied)
-        fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
-
-        # Output layer, class prediction
-        out = tf.layers.dense(fc1, n_classes)
-        # Because 'softmax_cross_entropy_with_logits' already apply softmax,
-        # we only apply softmax to testing network
-        out = tf.nn.softmax(out) if not is_training else out
-        '''
-        hidden = tf.layers.dense(inputs=x,
-                                 units=100,
-                                 activation=tf.nn.relu)
-        out = tf.layers.dense(hidden, 1)
+    
+        conv1    = tf.layers.conv2d(inputs=x,
+                                    filters=32,
+                                    kernel_size=5,
+                                    activation=tf.nn.relu)
+        maxpool1 = tf.layers.max_pooling2d(inputs=conv1,
+                                           pool_size=2,
+                                           strides=2)
+        conv2    = tf.layers.conv2d(inputs=maxpool1,
+                                    filters=32,
+                                    kernel_size=5,
+                                    activation=tf.nn.relu)
+        maxpool2 = tf.layers.max_pooling2d(inputs=conv2,
+                                           pool_size=2,
+                                           strides=2)
+        conv3    = tf.layers.conv2d(inputs=maxpool,
+                                    filters=32,
+                                    kernel_size=3,
+                                    activation=tf.nn.relu)
+        maxpool3 = tf.layers.max_pooling2d(inputs=conv2,
+                                           pool_size=2,
+                                           strides=2)
+        flat     = tf.contrib.layers.flatten(inputs=maxpool3)
+        dropout1 = tf.layers.dropout(inputs=flat,
+                                     rate=dropout,
+                                     training=true)
+        dense1   = tf.layers.dense(inputs=dropout1,
+                                   units=500,
+                                   activation=tf.nn.relu)
+        
+        dropout2 = tf.layers.dropout(inputs=fc1,
+                                     rate=dropout,
+                                     training=True)
+        out = tf.layers.dense(dropout2,
+                              units=1)
+        
+        #hidden = tf.layers.dense(inputs=x,
+        #                         units=100,
+        #                         activation=tf.nn.relu)
+        #out = tf.layers.dense(hidden, 1)
 
     return out
-
 
 # Because Dropout have different behavior at training and prediction time, we
 # need to create 2 distinct computation graphs that share the same weights.
@@ -108,10 +125,10 @@ def conv_net(x, reuse=False):
 # Create a graph for training
 logits_train = conv_net(X_train_batch)
 # Create another graph for testing that reuse the same weights
-#logits_test = conv_net(X)
+#logits_test = conv_net(X_test_batch)
 
 # Define loss and optimizer (with train logits, for dropout to take effect)
-loss_op   = tf.reduce_mean(tf.square(tf.subtract(Y_train_batch, logits_train)))
+loss_op   = tf.reduce_mean(tf.square(tf.subtract(Y_train_batch, logits_train))) #mean square error
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op  = optimizer.minimize(loss_op)
 
