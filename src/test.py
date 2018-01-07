3e735cbddf0a0823a84cf82821963629cae8a38c
tobiaj <tobiaj@kth.se>
2018-01-07 14:19:52 +0100

    Single Id cam fetch

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import json
 path = "/data"
 data = BytesIO()
 c = pycurl.Curl()
-c.setopt(c.URL, 'https://webcamstravel.p.mashape.com/webcams/list/category=traffic/limit=10,1/country=GB/nearby=51.5073509,-0.1277583,5?lang=en&show=webcams%3Aimage')
+c.setopt(c.URL, 'https://webcamstravel.p.mashape.com/webcams/list/webcam=1508414354?lang=en&show=webcams%3Aimage')
 c.setopt(c.WRITEFUNCTION, data.write)
 c.setopt(c.HTTPHEADER, ['X-Mashape-Key: MXoQyRzvhvmshzJOk2Oe9IweEpLfp1W1feUjsnLBt5yv7NJoVs'])
 c.perform()
